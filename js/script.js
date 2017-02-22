cd29a14f173dc8b36cce6eec3e47cc72ea1c92ef
sanjadoc <sanja_doc@ukr.net>
2017-02-22 17:20:12 +0200

    JQw3s-10-ajax request an response

--- js/script.js
+++ js/script.js
@@ -227,23 +227,55 @@ $(document).ready(function(){
 
 //AJAX 
 function loadDoc() {
-  var xhttp;
-  if (window.XMLHttpRequest) {
+    
+    var xhttp;
+    
+    if (window.XMLHttpRequest) {
     // code for modern browsers
-    xhttp = new XMLHttpRequest();
+        xhttp = new XMLHttpRequest();
     } else {
     // code for IE6, IE5
-    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo-ajax").innerHTML = this.responseText;
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-  };
-  xhttp.open("GET", "js/ajax_info.txt", true);
-  xhttp.send();
-}
+    
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo-ajax").innerHTML = this.responseText;
+        }
+    };
+    
+    xhttp.open("GET", "js/ajax_info.txt", true);
+    xhttp.send();
 
+    //xhttp.open("POST", "js/ajax_demo.asp", true);
+    //xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    //xhttp.send("fname=Henry&lname=Ford");    
+};
+
+
+//Ajax 2
+function ajaxTest() {
+    
+    var xhttp;
+    
+    if (window.XMLHttpRequest) {
+    // code for modern browsers
+        xhttp = new XMLHttpRequest();
+    } else {
+    // code for IE6, IE5
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+              document.getElementById("demo-ajax2").innerHTML = this.responseText;
+        }
+    };
+    
+    xhttp.open("GET", 'js/ajax_info.txt', true);
+    xhttp.send();
+    
+};
 
 
 
